372d3ef6bdaffa7d344e953addfd40fb1022d8ce
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2023-01-01 21:40:35 +0800

    hotfix not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,7 +9,7 @@ const NotFoundPage = () => {
   return (
     <div className="h-[60vh] py-20 flex flex-col items-center">
       <h2 className="font-black text-4xl text-red-800 mb-4">404 Not Found</h2>
-      <p>Oh uh.. looks like the page you're looking for doesn't exist</p>
+      <p>Oh uh.. looks like the page you&apos;re looking for doesn&apos;t exist</p>
       <Button
         onClick={() => router.push("/")}
         color="primary"
