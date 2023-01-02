47fdbd67904cbe30538340d01a0a2359c0a2f3ff
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2023-01-02 23:25:38 +0800

    remove image in Hero

--- src/components/heros/Hero.tsx
+++ src/components/heros/Hero.tsx
@@ -1,30 +1,16 @@
-import Image from "next/image";
-
 const Hero = () => {
   return (
-    <div className="md:px-10 md:py-20 p-2 flex md:flex-row flex-col-reverse items-center justify-center">
-      <div className="md:p-8 p-5 md:flex-[0.8]">
-        <h2 className="text-4xl font-black mb-2 text-red-800">
-          Checks over stripes.
-        </h2>
-        <p className="font-medium mb-2">
-          Marketplace for copping heat Nike sneakers 👟
-        </p>
-        <p className="text-zinc-500 text-[16px]">
-          (This is a site built with Next.js 13 Beta for experimenting and
-          learning purposes)
-        </p>
-      </div>
-      <div>
-        <Image
-          src="/images/thumbnail.png"
-          alt="thumbnail"
-          priority={true}
-          height={200}
-          width={200}
-          className="h-auto w-auto"
-        />
-      </div>
+    <div className="md:px-18 md:py-28 py-14 px-2 text-center">
+      <h2 className="md:text-7xl text-5xl font-black mb-5 text-red-800">
+        Checks over stripes.
+      </h2>
+      <p className="font-medium mb-2">
+        Marketplace for copping heat Nike sneakers 👟
+      </p>
+      <p className="text-zinc-500 text-[16px]">
+        (This is a site built with Next.js 13 Beta for experimenting and
+        learning purposes)
+      </p>
     </div>
   );
 };
