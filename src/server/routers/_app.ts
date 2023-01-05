454e94a4c6690b3be0e2383ac9bec56efd07d0cf
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2023-01-06 00:52:02 +0800

    comment trpc first

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,10 +1,11 @@
 import { z } from "zod";
 import { fetchProducts } from "../../handlers/fetchProducts";
-import { fetchProductSuggestions } from "../../handlers/fetchProductsByCategory";
-import { procedure, router } from "../trpc";
+// import { fetchProductSuggestions } from "../../handlers/fetchProductsByCategory";
+import { adminProcedure } from "../procedures";
+import { router } from "../trpc";
 
 export const appRouter = router({
-  getProducts: procedure
+  getProducts: adminProcedure
     .input(
       z.object({
         skip: z.number(),
@@ -15,23 +16,23 @@ export const appRouter = router({
       const { skip, take } = input;
       return await fetchProducts(skip, take, ctx.prisma);
     }),
-  suggestProducts: procedure
-    .input(
-      z.object({
-        categoryId: z.string(),
-        skipProductId: z.string().optional(),
-        limit: z.number().optional(),
-      })
-    )
-    .query(async ({ input, ctx }) => {
-      const { categoryId, skipProductId, limit } = input;
-      return await fetchProductSuggestions({
-        prisma: ctx.prisma,
-        categoryId,
-        skipProductId,
-        limit,
-      });
-    }),
+  // suggestProducts: procedure
+  //   .input(
+  //     z.object({
+  //       categoryId: z.string(),
+  //       skipProductId: z.string().optional(),
+  //       limit: z.number().optional(),
+  //     })
+  //   )
+  //   .query(async ({ input, ctx }) => {
+  //     const { categoryId, skipProductId, limit } = input;
+  //     return await fetchProductSuggestions({
+  //       prisma: ctx.prisma,
+  //       categoryId,
+  //       skipProductId,
+  //       limit,
+  //     });
+  //   }),
 });
 
 // export type definition of API
