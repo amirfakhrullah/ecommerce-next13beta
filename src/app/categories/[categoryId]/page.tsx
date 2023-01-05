9079c96954b53ce5225025c941fd6d10213ba2f2
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2023-01-06 01:19:37 +0800

    try fix page props type

--- src/app/categories/[categoryId]/page.tsx
+++ src/app/categories/[categoryId]/page.tsx
@@ -1,3 +1,4 @@
+import { NextPage } from "next";
 import { notFound } from "next/navigation";
 import { use } from "react";
 import Border from "../../../components/Border";
@@ -13,7 +14,7 @@ interface PageProps {
     categoryId: string;
   };
   searchParams: {
-    page?: any;
+    page?: string | string[];
   };
 }
 
@@ -24,11 +25,9 @@ const CategoryIdPage = ({
   const pageNum = typeof page === 'string' ? parseInt(page) : 1;
   const skip = pageNum > 1 ? (pageNum - 1) * PRODUCTS_PER_PAGE : undefined;
 
-  const { category, products } = use(fetchProductsByCategory(
-    categoryId,
-    PRODUCTS_PER_PAGE,
-    skip
-  ));
+  const { category, products } = use(
+    fetchProductsByCategory(categoryId, PRODUCTS_PER_PAGE, skip)
+  );
 
   if (!category) {
     return notFound();
