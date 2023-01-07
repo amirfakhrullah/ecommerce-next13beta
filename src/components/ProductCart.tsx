7e58bbd61155f757cacccaeb441d2048d5e01ba7
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2023-01-07 12:46:23 +0800

    set cart limit size

--- src/components/ProductCart.tsx
+++ src/components/ProductCart.tsx
@@ -7,16 +7,11 @@ import { IoIosAddCircle } from "react-icons/io";
 import { useCartContext } from "../providers/CartContextProvider";
 import { toast } from "react-hot-toast";
 import cn from "../helpers/cn";
+import { DEFAULT_SIZES, LIMIT_CART_SIZE } from "../constants";
 
 interface ProductCartProps {
   product: FullProductClient;
 }
-
-/**
- * Temporary used only
- * Haven't fully migrated the sizes yet
- */
-const DEFAULT_SIZES = ["9", "9.5", "10", "10.5", "11", "11.5", "12", "12.5"];
 
 const ProductCart = ({ product }: ProductCartProps) => {
   const { id, name, image, sizes } = product;
@@ -27,6 +22,11 @@ const ProductCart = ({ product }: ProductCartProps) => {
 
   const handleAddToCart = () => {
     if (!size) return;
+    if (cartItems.length >= LIMIT_CART_SIZE) {
+      return toast.error(
+        `You are allowed to add only ${LIMIT_CART_SIZE} items to the cart`
+      );
+    }
     setIsLoading(true);
     setCartItems([
       ...cartItems,
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,9 @@
 export const PRODUCTS_PER_PAGE = 12;
+
+/**
+ * Temporary used only
+ * Haven't fully migrated the sizes yet
+ */
+export const DEFAULT_SIZES = ["9", "9.5", "10", "10.5", "11", "11.5", "12", "12.5"];
+
+export const LIMIT_CART_SIZE = 5;
