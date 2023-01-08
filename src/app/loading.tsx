db6842759a6db26ad2e3631018fd008b0b2a6f5a
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2023-01-08 16:02:45 +0800

    fix styles for Loader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,7 @@
 import Loader from "../components/Loader";
 
 const Loading = () => {
-  return (
-    <div className="h-[60vh] py-20 w-full flex flex-row items-center justify-center">
-      <Loader />
-    </div>
-  );
+  return <Loader />;
 };
 
 export default Loading;
