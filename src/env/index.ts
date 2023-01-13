f54dc499cf6b2667aec1d93eecc81dc59b406109
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2023-01-13 22:51:52 +0800

    setup cron

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,17 +3,18 @@ import { getEnvSafely } from "./config";
 /**
  * For server-used only
  */
-// Next Auth
 const GITHUB_CLIENT_ID = getEnvSafely("GITHUB_CLIENT_ID");
 const GITHUB_CLIENT_SECRET = getEnvSafely("GITHUB_CLIENT_SECRET");
 const STRIPE_API_KEY = getEnvSafely("STRIPE_API_KEY");
 const STRIPE_WEBHOOK_SECRET = getEnvSafely("STRIPE_WEBHOOK_SECRET");
+const CRON_API_KEY = getEnvSafely("CRON_API_KEY");
 
 const env = {
   GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET,
   STRIPE_API_KEY,
   STRIPE_WEBHOOK_SECRET,
+  CRON_API_KEY,
 };
 
 export default env;
